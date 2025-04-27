1a9734890d068f2bb9b310062099a76f1b311f01
SohamBhandary <bhandarysoham27@gmail.com>
2025-04-27 14:31:36 +0530

    Variables and Datatypes Basic

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as  newer version
+// alert(3+3) we are using node js not browser
+
+
+// code redebility should be high
+console.log(3+3);
+console.log("javascript")
+
+
+let name="javascript"
+let age=18
+let isLoggedIn= false
+let state;
+// number -> 2 to the power of 53
+//bigint
+//string-> double quotes preffered
+// boolean-> true or false
+// null -> stand alone value hai
+// undefined -> 
+// symbol->uniue
+
+// object
+
+console.log(typeof state)
+// output og typeof null is object and undefined is undefined
