ed048a221a3e5d31fb3bd8bf7f58a1d16a37fa28
SohamBhandary <bhandarysoham27@gmail.com>
2025-04-28 22:39:30 +0530

    ConversionOps in JS

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,53 @@
+// let score="hey"
+// console.log(typeof 
+//     (score))
+
+// let valueInNumber= Number(score)// Type Casted
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+// "33"=>33
+//"3abc"=>NaN
+//true=>1 ;false=>1
+
+// let isLoggedIn=1
+// let isLoggedIn="hey"
+// let conversionResult=Boolean(isLoggedIn)
+// console.log(typeof (conversionResult))
+// console.log(conversionResult)
+
+//1->true 0=>false
+//""->"false"
+// "hey"=> true
+
+// let num =33
+// let convert= String(num)
+// console.log(convert)
+// console.log(typeof(convert))
+
+// *****************************OPERATIONS*************************//
+// let value=3
+// let negValue=- value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**2)
+
+// console.log("hello" +" "+ "world");
+
+// console.log("1"+"2");
+// console.log("1"+2);
+// console.log("1"+2+2)
+// console.log(1+2+2)
+// console.log(1+2+"2")
+
+// console.log(true)
+// let num1=num2=num3=2+2
+// console.log(num1)
+// console.log(num2)
+// console.log(num3)
+
