baf967543fdfb5822f6e914d1e81bf60fe0ed5c8
SohamBhandary <bhandarysoham27@gmail.com>
2025-04-29 21:20:11 +0530

    Numbers and Maths

---
+++ 01_Basics/05_NumsAndMath.js
@@ -0,0 +1,34 @@
+// const score=400
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+// const otherNumber=23.8966
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds=100000
+// console.log(hundreds.toLocaleString());
+
+//*********************************Maths *********************************/
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(4,2,1,7));
+// console.log(Math.max(4,2,1,7));
+console.log((Math.random()*10)+1);
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
+
+
+
