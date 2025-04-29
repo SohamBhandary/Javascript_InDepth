ba88445f87ec8c425e5845bf4e18bc1ecf0e6c9e
SohamBhandary <bhandarysoham27@gmail.com>
2025-04-29 23:54:21 +0530

    Arrays part1

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,33 @@
+const myArr=[0,1,2,3,true,"Soham"]
+// const myHeros=["Shaktiman","Spiderman"]
+// console.log(myArr[4]);
+// console.log(myHeros[1]);
+//Array Methods
+
+// myArr.push("six")
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+// myArr.unshift(1)
+// console.log(myArr);
+// myArr.shift(1)
+// console.log(myArr);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(true));
+// const newArr= myArr.join()
+// console.log( typeof newArr)
+// slice and splice
+console.log("A",myArr);
+const myarr1=myArr.slice(1,3)
+console.log(myarr1);
+const myarr2=myArr.splice(1,3)
+console.log(myarr2);
+
+
+
+
+
+
+
+
