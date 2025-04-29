00f70ea14b794c0a65bb0baab665d152958ed47c
SohamBhandary <bhandarysoham27@gmail.com>
2025-04-30 00:20:38 +0530

    Arrays part 2 done

---
+++ 02_Basics/02_Arrays.js
@@ -0,0 +1,21 @@
+// const marvelHeros=["ironman","thor","hulk"]
+// const dcHeros=["superman","flash","batman"]
+
+// const allHeros= marvelHeros.concat(dcHeros)
+// console.log(allHeros);
+
+// const allNewHeros=[...marvelHeros,...dcHeros]
+// console.log(allHeros);
+// console.log(allHeros[5]);
+// const another_arr=[1,2,3,[4,5,6],7,[1,2,[1,2]]]
+// const realAnother=another_arr.flat(Infinity)
+// console.log(realAnother);
+
+// console.log( Array.isArray("Soham"))
+// console.log( Array.from ("Soham"))
+// console.log(Array.from({name:"hitesh"}));
+
+let score1=20
+let score2=30
+let score3=40
+console.log(Array.of(score1,score2,score3));
