82cc2fac18ecb02577dbb4f77bb919aee5b18cb4
SohamBhandary <bhandarysoham27@gmail.com>
2025-06-06 16:23:54 +0530

    Scope level mini hoisting

--- 03_Bascis/02_Scopes.js
+++ 03_Bascis/02_Scopes.js
@@ -1,20 +1,59 @@
-let a=20
+// let a=20
 
 
-if(true){
-    let a=2
-const b=20
-var c=30
-console.log(a);
-console.log(b);
+// if(true){
+//     let a=2
+// const b=20
+// var c=30
+// console.log(a);
+// console.log(b);
 
 
 
 
-}
+// }
 
- console.log(a);
+//  console.log(a);
 // console.log(b);
 // console.log(c);
 
 
+// function one(){
+//     const username="Soham"
+//     function two(){
+//         const website="youtube"
+//         console.log(username);
+        
+//     }
+//     // console.log(webiste);
+//     two();
+    
+// }
+// one()
+
+if(true){
+
+    const username="Soham"
+    if(username==="Soham"){
+        const website=" youtube"
+        console.log(username+website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+// +++++++++++++++++++++++++++++++++++++++++++++++Interesting+++++++++++++++++++++++++++++
+
+function  addOne(val){
+    return val+1;
+
+}
+console.log(addOne(5));
+
+
+
+const addTwo=function(num){
+    return num+2
+}
+console.log(addTwo(5));
