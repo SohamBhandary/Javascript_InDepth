15cb5e6c23a833cd1baec9e7c683239f9749c4c1
SohamBhandary <bhandarysoham27@gmail.com>
2025-06-06 16:51:26 +0530

    Arrow Function

---
+++ 03_Bascis/03_Arrow.js
@@ -0,0 +1,36 @@
+const user={
+
+    username:"Soham",
+    price:999,
+    welcomeMessage:function(){
+        console.log(`${this.username},welcome to website`);
+        console.log(this);
+        
+
+    }
+}
+// user.welcomeMessage()
+// user.username="java"
+// user.welcomeMessage()
+// function coffie (){
+//     console.log(this);
+    
+
+// }
+// coffie()
+// const chai=()=>{
+
+//     console.log("Chai");
+    
+// }
+// chai();
+
+const addTwo=(num1,num2)=>{
+    console.log((num1+num2));
+    
+}
+addTwo(2,2)
+
+
+
+
