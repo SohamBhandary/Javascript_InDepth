ea4b5729bd386367ceb4557c5b94b8fdc258c2c7
SohamBhandary <bhandarysoham27@gmail.com>
2025-06-08 19:18:08 +0530

    Switch Case Done

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,38 @@
+const month=9
+switch(month){
+    case 1:
+        console.log("Jan");
+        
+
+    break;
+
+    case 2:
+        console.log("Feb");
+        
+
+    break;
+    case 3:
+        console.log("mar");
+        
+
+    break;
+    case 4:
+        console.log("Arpil");
+        
+
+    break;
+    case 5:
+        console.log("May");
+        
+
+    break;
+    case 6:
+        console.log("June");
+        
+
+    break;
+    default:
+        console.log("This is deafult case");
+    break;
+        
+}
