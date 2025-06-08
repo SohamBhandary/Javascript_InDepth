2dee49fa2c326e29cf1fe296fe279b7130bc746a
SohamBhandary <bhandarysoham27@gmail.com>
2025-06-08 18:56:37 +0530

    Cotrol Flow Basic

---
+++ 04_controlflow/one.js
@@ -0,0 +1,28 @@
+// if statement
+// const isUserloggedin=true
+// const temp=60
+// if(temp<50){
+//     console.log("under 50");
+    
+
+
+// }
+// else{
+//     console.log("exceeded 50");
+    
+// }
+// <,>,>=,<=,==,===,!=
+
+// const score =200
+// if(score>100){
+// const power="fly"
+// console.log(power);
+
+
+// }
+
+const balance=1000
+if(balance>500){
+    console.log("Test");
+    
+}
