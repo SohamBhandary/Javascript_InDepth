89445d06e82ae7b593e4ef0d1c4d0ce54098fe67
SohamBhandary <bhandarysoham27@gmail.com>
2025-06-09 13:19:16 +0530

    truthy falsy and ternanry

---
+++ 04_controlflow/truthy.js
@@ -0,0 +1,32 @@
+// const userEmail="Soham@gmail.com"
+// if(userEmail){
+//     console.log("Got user Email");
+    
+// }
+// else{
+//     console.log("Dont have user email");
+    
+// }
+
+// // falsy values-> false,,0,,-0,BigInt,"",null,undefined,NaN
+// // truthy values -> "0","false"," ",[],{},funtion(){}
+
+// const emptyObj={}
+// if(Object.keys(emptyObj===0)){
+//     console.log("Object is empty");
+    
+// }
+
+//Nullish Coalescing Ops(??): null defined
+let val1;
+// val1=5??10
+// val1=null??10
+
+// console.log(val1);
+
+//terniary ops
+//condition ? true:false
+
+const iceTeaPrice=100
+iceTeaPrice>90?console.log("less"):console.log("more");
+
