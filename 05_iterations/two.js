b80157efb21dfbf1727ee37f0adae7d0ccd479fc
SohamBhandary <bhandarysoham27@gmail.com>
2025-06-10 10:46:42 +0530

    Do While and while

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+
+let i=0;
+while (i<=10) {
+    console.log(i);
+    i++;
+    
+    
+}
+
+let j=0;
+do{
+    console.log(j);
+    j++;
+
+    
+}while(j<=10);
+let myArr=[1,2,3,4]
+let index=0;
+while(index<=myArr.length){
+    console.log(index);
+ index++;
+    
+}
