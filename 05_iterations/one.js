010aecc817b74201ab5d595e70be90431df9d247
SohamBhandary <bhandarysoham27@gmail.com>
2025-06-10 10:32:34 +0530

    For loop break continue

---
+++ 05_iterations/one.js
@@ -0,0 +1,41 @@
+// //for
+// for (let index = 0; index < 10; index++) {
+//       if(index == 5){
+//         console.log("5 is the best num");
+        
+//     }
+//     console.log(index);
+  
+    
+    
+// }
+
+// for (let index = 0; index <= 10; index++) {
+//     console.log(`Outer loop index: ${index}`);
+    
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`  Inner loop j: ${j}`);
+//     }
+// }
+
+// let myArr=[1,2,3,4,5]
+// for (let index = 0; index < myArr.length; index++) {
+//     console.log(myArr[index]);
+    
+    
+// }
+//break and continue
+ for (let index = 0; index <= 10; index++) {
+
+    // if(index===5){
+    //     continue;
+    // }
+    
+    if(index===5){
+        break;
+    }
+
+    console.log(index);
+    
+    
+ }
