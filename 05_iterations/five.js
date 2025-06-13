e756545c739719bb07aab24c9f51e4e595602fa0
SohamBhandary <bhandarysoham27@gmail.com>
2025-06-13 19:29:25 +0530

    DOM Basic

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,18 +1,22 @@
-const coding =["cpp","java","js","pyhton"]
-coding.forEach(element => {
-    // console.log(element);
+// const coding =["cpp","java","js","pyhton"]
+// coding.forEach(element => {
+//     // console.log(element);
     
     
-});
-const myCoding=[{
-    lan:"js",
-    lanname:".js"
-},{
-    lan:"java",
-    lanname:".java"
-},{
-    lan:"python",
-    lanname:".py"
-}]
-myCoding.forEach((item)=>{console.log(item.lan);
+// });
+// const myCoding=[{
+//     lan:"js",
+//     lanname:".js"
+// },{
+//     lan:"java",
+//     lanname:".java"
+// },{
+//     lan:"python",
+//     lanname:".py"
+// }]
+// myCoding.forEach((item)=>{console.log(item.lan);
+// })
+
+const myArr =[1,2,3,4,5,6]
+myArr.forEach((item)=>{console.log( item*2);
 })
